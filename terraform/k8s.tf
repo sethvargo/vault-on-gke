9d95a052b0a44b57ffc54012af0c9a46eb8d8bae
Steven Aldinger <stevenaldinger@gmail.com>
2021-02-04 14:48:21 -0500

    fix  argument named load_config_file is not expected here

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -4,8 +4,7 @@ data "google_client_config" "current" {}
 
 # This file contains all the interactions with Kubernetes
 provider "kubernetes" {
-  load_config_file = false
-  host             = google_container_cluster.vault.endpoint
+  host = google_container_cluster.vault.endpoint
 
   cluster_ca_certificate = base64decode(
     google_container_cluster.vault.master_auth[0].cluster_ca_certificate,
